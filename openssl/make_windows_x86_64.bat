9f2cec086d599809d8acef25397bc1887b5543f7
Sun Daowen <sundaowen@gmail.com>
2016-11-19 11:26:36 +0800

    windows

---
+++ openssl/make_windows_x86_64.bat
@@ -0,0 +1,41 @@
+IF DEFINED VS90COMNTOOLS (
+  SET VCVARSALL="%VS90COMNTOOLS%..\..\VC\vcvarsall.bat"
+) ELSE IF DEFINED VS120COMNTOOLS (
+  SET VCVARSALL="%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
+) ELSE IF DEFINED VS110COMNTOOLS (
+  SET VCVARSALL="%VS110COMNTOOLS%..\..\VC\vcvarsall.bat"
+) ELSE IF DEFINED VS100COMNTOOLS (
+  SET VCVARSALL="%VS100COMNTOOLS%..\..\VC\vcvarsall.bat"
+)
+SET target_lib_suffix=
+IF NOT DEFINED VCVARSALL (
+  ECHO Can not find VC2008 or VC2010 or VC2012 or VC2013 installed!
+  GOTO ERROR
+)
+CALL %VCVARSALL% amd64
+@ECHO ON
+SET rootdir=%~dp0
+SET cwdir=%CD%
+SET target=windows_x86_64
+SET prefix=%rootdir%%target%
+SET openssldir=%prefix%\ssl
+SET /P version=<"%rootdir%version.txt"
+RD /S /Q "%rootdir%%version%"
+MD "%rootdir%%version%"
+XCOPY "%rootdir%..\%version%" "%rootdir%%version%" /S /Y
+CD /D "%rootdir%%version%"
+perl Configure VC-WIN64A no-shared no-asm no-dso --prefix="%prefix%" --openssldir="%openssldir%"
+CALL ms\do_win64a.bat
+nmake -f ms\nt.mak
+nmake -f ms\nt.mak install
+MD "%rootdir%..\target\include\%target%"
+XCOPY "%prefix%\include" "%rootdir%..\target\include\%target%" /S /Y
+MD "%rootdir%..\target\lib\%target%%target_lib_suffix%"
+COPY /Y "%prefix%\lib\libeay32.lib" "%rootdir%..\target\lib\%target%%target_lib_suffix%"
+CD /D "%cwdir%"
+RD /S /Q "%rootdir%%version%"
+RD /S /Q "%prefix%"
+GOTO :EOF
+
+:ERROR
+PAUSE
